998baf27af114e800c9f496176f9e57563177a5f
devnana222 <devnana222@gmail.com>
2025-01-28 00:03:55 +0900

    연관관계 매핑 시작22

--- src/main/java/jpabook/jpashop/domain/JpaMain.java
+++ src/main/java/jpabook/jpashop/domain/JpaMain.java
@@ -8,6 +8,29 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Order order = new Order();
+            em.persist(order);
+            //방법 1
+            order.addOrderItem(new OrderItem());
+
+            //방법 2. Member의 orderList - Order의 orderItems의 연결까지 만들지 않아도 되긴함.
+//            OrderItem orderItem = new OrderItem();
+//            orderItem.setOrder(order);
+//            em.persist(orderItem);
+
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         //code
 
         em.close();
